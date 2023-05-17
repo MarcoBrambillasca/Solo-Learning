bd6a84fa691fa444109281f3464d71cc65d98467
Marco Andrea Brambillasca <126414279+MarcoBrambillasca@users.noreply.github.com>
2023-05-17 10:39:57 +0200

    Update

---
+++ Progetti in C++ (basic)/Input output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int numero;
+
+    std::cout << "Inserisci un numero: ";
+    std::cin >> numero;
+
+    std::cout << "Hai inserito: " << numero << std::endl;
+
+    return 0;
+}
