c4910c2ee70796f1afd637570069147cb30cec73
Marco Andrea Brambillasca <126414279+MarcoBrambillasca@users.noreply.github.com>
2023-05-17 10:04:35 +0200

    Update Scanf.h

--- Progetti in C/Scanf.h
+++ Progetti in C/Scanf.h
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main() {
+  int intero;
+
+    printf("Inserisci un intero: ");
+    scanf("%d", &intero);
+
+    printf("Hai inserito: %d\n", intero);
+  
+  
+  float float;
+
+    printf("Inserisci un numero con la virgola: ");
+    scanf("%f", &float);
+
+    printf("Hai inserito: %.2f\n", float);
+
+  
+  char parola[20];
+
+    printf("Inserisci una parola: ");
+    scanf("%s", parola);
+
+    printf("Parola inserita: %s!\n", parola);
+
+    return 0;
+  
+  
+}
